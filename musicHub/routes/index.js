ac1a26b70f020959c3e6dc709c403a6f7b332a3b
PeterStanger92020362 <65217016+PeterStanger92020362@users.noreply.github.com>
2022-04-11 20:12:37 +1200

    start 0auth

--- musicHub/routes/index.js
+++ musicHub/routes/index.js
@@ -1,20 +1,11 @@
 var express = require('express');
-var router = express.Router();
-
-var getData = function () {
-  var data = {
-      'item1': 'https://images.unsplash.com/photo-1563422156298-c778a278f9a5',
-      'item2': 'https://images.unsplash.com/photo-1620173834206-c029bf322dba',
-      'item3': 'https://images.unsplash.com/photo-1602491673980-73aa38de027a'
-  }
-  return data;
-}
+var app = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MusicHub', "data": getData() });
+app.get('/', function(req, res) {
+  res.render('index', { title: 'MusicHub' });
 });
 
 
 
-module.exports = router;
+module.exports = app;
