a5afb92a5d121aacd84319b04ffe333f7573f18d
PeterStanger92020362 <65217016+PeterStanger92020362@users.noreply.github.com>
2022-04-12 19:01:28 +1200

    fixed code to now prompt for GET /callback

--- musicHub/app.js
+++ musicHub/app.js
@@ -29,6 +29,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.locals.basedir = app.get('views');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -39,9 +41,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+//app.use(function(req, res, next) {
+//  next(createError(404));
+//});
 
 // error handler
 app.use(function(err, req, res, next) {
--- musicHub/routes/index.js
+++ musicHub/routes/index.js
@@ -1,11 +1,21 @@
-var express = require('express');
-var app = express.Router();
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 /* GET home page. */
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.render('index', { title: 'MusicHub' });
 });
 
+app.get('/login', (req, res) => {
+  res.redirect('https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID + '&response_type=code&redirect_uri=' + REDIRECT_URI + '&show_dialog=true');
+})
 
 
 module.exports = app;
